a066133d2d0e977a127538201d1eb53e5e5443ef
Jason Hall <jason@chainguard.dev>
2025-01-10 22:28:23 -0500

    back to implicit flow, lots of minor improvements, still doesn't work
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-# Work in Progress
+# Verifiable Screenshot Chrome Extension
+
+With this extension, you can take a screenshot of a webpage with Cmd+Shift+6, and then the extension attests a digest of the screenshot to Rekor, using an ephemeral cert from Fulcio, tied to your identity.
+
+This proves that you took the screenshot at a certain time, and that if the image ever changes there's no way to forge the attestation.
+
+_This doesn't currently work_.
+
+-----
 
 Notes for how to get this working:
 
@@ -10,9 +18,9 @@ Notes for how to get this working:
 So far this extension:
 
 1. listens for `cmd+shift+6` to take a screenshot of the current tab
+1. does Google OAuth to get an ID token
 1. generates an ephemeral public/private keypair
-1. does Google OAuth to get an id token
-1. signs the token's `sub` with the private key
+1. signs the ID token's `sub` with the private key
 1. posts the public key, token and signed `token.sub` to Fulcio
 1. 💥 fails with `"There was an error processing the identity token"` presumably because the id token doesn't have audience of `sigstore` -- but setting `audience` in the oauth request gets blocked by Google...
 
@@ -22,3 +30,4 @@ Once I get past that I think the next step is to:
 1. take the digest of the screenshot data, wrap the digest in the correct envelope(s)
 1. post to Rekor
 1. get the log ID returned by Rekor, and redirect to search.sigstore.dev, and download the screenshot data.
+1. show verification instructions.
--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,9 @@
+
 // https://stackoverflow.com/questions/38552003/how-to-decode-jwt-token-in-javascript-without-using-a-library
 function parseJwt(token) {
-    var base64Url = token.split('.')[1];
-    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
-    var jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
+    let base64Url = token.split('.')[1];
+    let base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    let jsonPayload = decodeURIComponent(atob(base64).split('').map(function (c) {
         return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
     }).join(''));
     return JSON.parse(jsonPayload);
@@ -10,75 +11,96 @@ function parseJwt(token) {
 
 function base64(arrayBuffer) { return btoa(String.fromCharCode.apply(null, new Uint8Array(arrayBuffer))); }
 
-function useKey(key) {
-    console.log("pub", key.publicKey);
-    console.log("priv", key.privateKey);
+function randString() { return (Math.random() + 1).toString(36).substring(3); }
 
-    var manifest = chrome.runtime.getManifest();
-    var clientId = encodeURIComponent(manifest.oauth2.client_id);
-    var scopes = encodeURIComponent(manifest.oauth2.scopes.join(' '));
-    var redirectUri = encodeURIComponent(`https://${chrome.runtime.id}.chromiumapp.org`);
+// https://developers.google.com/identity/openid-connect/openid-connect#exchangecode
+function getIDToken() {
+    chrome.identity.getProfileUserInfo(info => {
+        console.log("info", info);
+        let manifest = chrome.runtime.getManifest();
+        let state = randString();
+        let params = new URLSearchParams();
+        params.append("client_id", "344413968647-31l50a3vmasmh1p59rps3fjv2o3ncrae.apps.googleusercontent.com");
+        params.append("response_type", "id_token");
+        params.append("nonce", randString());
+        params.append("redirect_uri", `https://${chrome.runtime.id}.chromiumapp.org`);
+        params.append("scope", "https://www.googleapis.com/auth/userinfo.email");
+        params.append("state", state);
+        params.append("login_hint", info.email);
+        let url = `https://accounts.google.com/o/oauth2/v2/auth?` + params
 
-    chrome.identity.launchWebAuthFlow({
-        url: `https://accounts.google.com/o/oauth2/auth?client_id=${clientId}&response_type=id_token&access_type=offline&redirect_uri=${redirectUri}&scope=${scopes}`,
-        interactive: true
-    }, function (redirectedTo) {
-        if (chrome.runtime.lastError) {
-            console.error(chrome.runtime.lastError.message);
-        }
-        else {
-            // Example: id_token=<YOUR_BELOVED_ID_TOKEN>&authuser=0&hd=<SOME.DOMAIN.PL>&session_state=<SESSION_STATE>&prompt=<PROMPT>
-            var response = redirectedTo.split('#', 2)[1];
-            var idToken = response.split('&', 1)[0].split('=', 2)[1];
-            useToken(key, idToken);
-        }
+        chrome.identity.launchWebAuthFlow({
+            url: url,
+            interactive: true
+        }, function (redirectedTo) {
+            let params = new URLSearchParams(redirectedTo.split('#')[1]);
+            console.log("params", Object.fromEntries(params));
+            let gotState = params.get('state');
+            if (gotState !== state) {
+                console.error(`state mismatch ${gotState} !== ${state}`);
+                return;
+            }
+
+            let idToken = params.get('id_token');
+            useToken(idToken);
+        });
     });
 }
 
-function useToken(key, token) {
-    console.log("token", token);
-    let parsed = parseJwt(token);
+function useToken(idToken) {
+    let parsed = parseJwt(idToken);
     console.log("parsed", parsed);
 
-    crypto.subtle.sign("RSASSA-PKCS1-v1_5", key.privateKey, new TextEncoder().encode(parsed.sub)).then(signature => {
-        useSignature(key, token, signature);
-    }).catch(console.error);
-}
-
-function useSignature(key, token, signature) {
-    console.log("signature", signature);
-    console.log("signature b64", base64(signature));
-
-    fetch("https://fulcio.sigstore.dev/api/v2/signingCert", {
-        method: "POST",
-        headers: { "Authorization": `Bearer ${token}` },
-        body: JSON.stringify({
-            credentials: { oidcIdentityToken: token },
-            publicKeyRequest: {
-                publicKey: {
-                    algorithm: "RSASSA-PKCS1-v1_5",
-                    content: base64(new TextEncoder().encode(key.publicKey)),
-                },
-                proofOfPossession: base64(signature),
-            }
-        }),
-    }).then(useResponse).catch(console.error); // TODO: This fails due to CORS
-}
-
-function useResponse(resp) {
-    console.log("resp", resp);
-    console.log("resp", resp.json().then(console.log).catch(console.error));
-}
-
-function capture(tab) {
-    console.log("tab", tab);
-
+    // Generate an ephemeral key, use it to sign the `sub` so we can prove we own the private key.
     crypto.subtle.generateKey({
         name: "RSASSA-PKCS1-v1_5",
         modulusLength: 2048,
         publicExponent: new Uint8Array([0x01, 0x00, 0x01]),
         hash: { name: "SHA-256" },
-    }, false, ["sign", "verify"]).then(useKey).catch(console.error);
+    }, false, ["sign", "verify"]).then(key => {
+        console.log("pub", key.publicKey);
+        console.log("priv", key.privateKey);
+        crypto.subtle.sign("RSASSA-PKCS1-v1_5", key.privateKey, new TextEncoder().encode(parsed.sub)).then(signature => {
+            getCert(key, idToken, signature);
+        }).catch(console.error);
+    }).catch(console.error);
+}
+
+function getCert(key, idToken, signature) {
+    console.log("signature", signature);
+    let body = {
+        credentials: { oidcIdentityToken: idToken },
+        publicKeyRequest: {
+            publicKey: {
+                algorithm: "RSASSA-PKCS1-v1_5",
+                content: base64(new TextEncoder().encode(key.publicKey)),
+            },
+            proofOfPossession: base64(signature),
+        }
+    };
+    console.log("body", body);
+
+    fetch("https://fulcio.sigstore.dev/api/v2/signingCert", {
+        method: "POST",
+        body: JSON.stringify(body),
+    }).then(resp => {
+        console.log("resp", resp);
+        resp.json().then(body => {
+            if (!resp.ok) {
+                console.error("body", body);
+                return;
+            }
+            console.log("body", body);
+
+            // TODO: use ephemeral cert.
+        }).catch(console.error);
+    }).catch(console.error);
+}
+
+function capture(tab) {
+    // console.log("tab", tab);
+
+    getIDToken();
 }
 
 chrome.action.onClicked.addListener((tab) => { chrome.tabs.captureVisibleTab({ format: "png" }, capture); });
